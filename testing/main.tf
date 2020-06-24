5dbe2c483702b8b87a5a6057c0be9c2a5d4758e0
canliuuu <53069898+canliuuu@users.noreply.github.com>
2020-06-24 14:00:37 +0800

    Create main.tf

---
+++ testing/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region="us-east-1"
+}
+
+variable "instance_type" {
+  description = "あアｱ１1ａa中表鷗㊞屢울가繁體áé丵-g11n-AWS123"
+  default     = "t2.micro"
+}
+
+variable "department" {
+  description = "あアｱ１1ａa中表鷗㊞屢울가繁體áé丵-g11nDepartment tag"
+}
+
+resource "aws_instance" "machine1" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        department = var.department
+    }
+}
+
+resource "aws_instance" "machine2" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        department = var.department
+    }
+}
