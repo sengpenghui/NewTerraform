fc0b1fd419439d6d61fc4176e17ad796064b425f
sengpenghui <34117349+sengpenghui@users.noreply.github.com>
2020-08-19 23:23:24 -0700

    Change VM 名字
    修改VM的名字

--- あアｱ１1ａa中表鷗㊞屢울가繁體áé丵-g11n-awsF/main.tf
+++ あアｱ１1ａa中表鷗㊞屢울가繁體áé丵-g11n-awsF/main.tf
@@ -11,7 +11,7 @@ variable "department" {
   description = "あアｱ１1ａa中表鷗㊞屢울가繁體áé丵-g11nDepartment tag"
 }
 
-resource "aws_instance" "體あ丵울겨éäåA1" {
+resource "aws_instance" "sph體あ丵울겨éäåA1" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
@@ -21,7 +21,7 @@ resource "aws_instance" "體あ丵울겨éäåA1" {
     }
 }
 
-resource "aws_instance" "體あ丵울겨éäåA2" {
+resource "aws_instance" "sph體あ丵울겨éäåA2" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
